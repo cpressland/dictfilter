3dd99732ffd6fe89f9cc25689f5873502d2bcb41
Chris Latham <backwardspy@gmail.com>
2018-07-27 11:17:49 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='dictfilter',
-    version='1.1.1',
+    version='1.1.2',
     author='Chris Latham',
     author_email='backwardspy@gmail.com',
     description='Filter dictionaries based on a list of field names.',
